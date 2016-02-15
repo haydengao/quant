348ccec24814d8d236bcb1663b24350059666e52
lixvow <lixvow@gmail.com>
2016-02-15 10:47:27 +0800

    jj

--- flask_ajax.py
+++ flask_ajax.py
@@ -15,8 +15,11 @@ def ajax_request():
     date = request.form['date']
     date = datetime.date(date[0:3],date[5:6],date[8:9])
     pt = ma.Position.query.filter_by(strategy_id=1,date_id=Survey.query.filter_by(date=date).first().id).first()
-    return jsonify({'id':pt.id, 'ticker':pt.ticker, 'name':pt.name, 'amount':pt.amount, 'cost':pt.cost, 'price':pt.price, 'value':pt.value, 'increase':pt.increase, 'weight':pt.weight})
-    
+    if pt == None:
+        return jsonify({'name':999})
+    else:
+        return jsonify({'id':pt.id, 'ticker':pt.ticker, 'name':pt.name, 'amount':pt.amount, 'cost':pt.cost, 'price':pt.price, 'value':pt.value, 'increase':pt.increase, 'weight':pt.weight})
+
     
 if __name__ == "__main__":
     app.run(debug = True)

f42503e57ccb2897399fe5f41700e38a98f16bf5
lixvow <lixvow@gmail.com>
2016-02-15 10:57:14 +0800

    jj

--- flask_ajax.py
+++ flask_ajax.py
@@ -13,8 +13,9 @@ def index():
 @app.route('/ajax', methods = ['POST'])
 def ajax_request():
     date = request.form['date']
-    date = datetime.date(date[0:3],date[5:6],date[8:9])
+    
     '''
+    date = datetime.date(date[0:3],date[5:6],date[8:9])
     pt = ma.Position.query.filter_by(strategy_id=1,date_id=Survey.query.filter_by(date=date).first().id).first()
     if pt == None:
         return jsonify({'name':999})

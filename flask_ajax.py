afa9a319911f4eed0e8f02922dc44cb8404cbe6a
lixvow <lixvow@gmail.com>
2016-02-15 13:15:19 +0800

    jj

--- flask_ajax.py
+++ flask_ajax.py
@@ -13,16 +13,16 @@ def index():
 @app.route('/ajax', methods = ['POST'])
 def ajax_request():
     date = request.form['date']
-    '''
-    date = datetime.date(date[0:3],date[5:6],date[8:9])
-    pt = ma.Position.query.filter_by(strategy_id=1,date_id=Survey.query.filter_by(date=date).first().id).first()
+    formatDate = datetime.date(date[0:4],date[5:7],date[8:10])
+    
+    pt = ma.Position.query.filter_by(strategy_id=1,date_id=Survey.query.filter_by(date=formatDate).first().id).first()
     if pt == None:
         return jsonify({'name':999})
     else:
         return jsonify({'id':pt.id, 'ticker':pt.ticker, 'name':pt.name, 'amount':pt.amount, 'cost':pt.cost, 'price':pt.price, 'value':pt.value, 'increase':pt.increase, 'weight':pt.weight})
-'''
 
-    return jsonify({'name0':date[0:4],'name1':date[5:7],'name2':date[8:10]})
+
+#   return jsonify({'name0':date[0:4],'name1':date[5:7],'name2':date[8:10]})
     
 if __name__ == "__main__":
     app.run(debug = True)

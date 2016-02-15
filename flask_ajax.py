cf019a0f241d59ffa1da1095054e0baa4349e6c2
lixvow <lixvow@gmail.com>
2016-02-15 10:24:39 +0800

    jj

--- flask_ajax.py
+++ flask_ajax.py
@@ -1,4 +1,5 @@
 from flask import (Flask, request, jsonify, render_template)
+import datetime
 import myapp as ma
                    
 app = Flask(__name__)
@@ -12,7 +13,9 @@ def index():
 @app.route('/ajax', methods = ['POST'])
 def ajax_request():
     date = request.form['date']
-    return jsonify(date=date)
+    date = datetime.date(date[0:3],date[5:6],date[8:9])
+    pt = ma.Position.query.filter_by(strategy_id=1,date_id=Survey.query.filter_by(date=date).first().id).first()
+    return jsonify({'id':pt.id, 'ticker':pt.ticker, 'name':pt.name, 'amount':pt.amount, 'cost':pt.cost, 'price':pt.price, 'value':pt.value, 'increase':pt.increase, 'weight':pt.weight})
     
     
 if __name__ == "__main__":

d2a2d35d390f1d2c8cd9ea832e46eb19979e4a04
lixvow <lixvow@gmail.com>
2016-02-16 18:42:11 +0800

    aa

--- myapp.py
+++ myapp.py
@@ -100,14 +100,6 @@ class Benchmark(db.Model):
         return '<Benchmark %r>' % self.date
 
 
-
-
-class ExampleForm(Form):
-    dt = DateField('DatePicker', format='%Y-%m-%d')
-
-
-
-
 #app.debug = True
 
 @app.route('/')
@@ -193,14 +185,58 @@ def strategy(name):
 
         today = today.strftime('%Y-%m-%d')
 
-        return render_template('strategy.html',name=name,survey = survey,positions = positions,transfer = transfer,date = today,form1 = form1)
+        return render_template('strategy.html',name=name,survey = survey,positions = positions,transfer = transfer,today = today)
+
+
+@app.route('/positions', methods = ['POST'])
+def positions_ajax_request():
+    date = request.form['date']
+    formatDate = datetime.date(int(date[0:4]),int(date[5:7]),int(date[8:10]))
+    dateID = Survey.query.filter_by(date=formatDate).first()
+    if dateID != None:
+        pt_ = Position.query.filter_by(strategy_id=1,date_id=dateID.id).all()
+        if pt_ == None:
+            return jsonify({'name':"该日没数据"})
+        else:
+            pt = list(range(len(pt_)))
+            
+            for i in range(len(pt_)):
+                pt[i] = {'ticker':pt_[i].ticker, 'name':pt_[i].name, 'amount':pt_[i].amount, 'cost':pt_[i].cost, 'price':pt_[i].price, 'value':pt_[i].value, 'increase':pt_[i].increase, 'weight':pt_[i].weight}
+            
+            return jsonify(results=pt)
+    else:
+        return jsonify({'name':"该日没数据"})
 
 
 
-@app.route('/test', methods=['POST','GET'])
-def testForm():
-    datee = '2013-01-03'
-    form = ExampleForm()
-    if form.validate_on_submit():
-        datee = form.dt.data.strftime('%Y-%m-%d')
-    return render_template('dp.html', form=form, datee = datee)
+@app.route('/transfers', methods = ['POST'])
+def transfers_ajax_request():
+    date = request.form['date']
+    formatDate = datetime.date(int(date[0:4]),int(date[5:7]),int(date[8:10]))
+    dateID = Survey.query.filter_by(date=formatDate).first()
+    if dateID != None:
+        ts_ = Transfer.query.filter_by(strategy_id=1,date_id=dateID.id).all()
+        if ts_ == None:
+            return jsonify({'name':"该日没数据"})
+        else:
+            ts = list(range(len(ts_)))
+            
+            for i in range(len(ts_)):
+                deal_amount = '--'
+                deal_time = '--'
+                cost = '--'
+                if ts_[i].dealAmount != None:
+                    deal_amount = ts_[i].dealAmount
+                
+                if ts_[i].dealTime != None:
+                    deal_time = ts_[i].dealTime.strftime('%H:%M:%S')
+                
+                if ts_[i].cost != None:
+                    cost = ts_[i].cost
+                
+                ts[i] = {'ticker':ts_[i].ticker, 'name':ts_[i].name, 'direction':ts_[i].direction, 'orderAmount':ts_[i].orderAmount, 'dealAmount':deal_amount, 'orderTime':ts_[i].orderTime.strftime('%H:%M:%S'), 'dealTime':deal_time, 'cost':cost, 'status':ts_[i].status}
+            
+            return jsonify(results=ts)
+    else:
+        return jsonify({'name':"该日没数据"})
+

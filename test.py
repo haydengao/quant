28f27dde4df586237ae652d5c3f7e21e48a26866
lixvow <lixvow@gmail.com>
2016-02-22 14:50:46 +0800

    oo

--- test.py
+++ test.py
@@ -4,7 +4,7 @@ from flask import (Flask, render_template, request, jsonify)
 app = Flask(__name__)
 
 @app.route('/')
-def strategy():
-    listt = [{'a':'1','b':'2','c':'3'},{'a':'4','b':'5','c':'6'}]
+def index():
+    #listt = [{'a':'1','b':'2','c':'3'},{'a':'4','b':'5','c':'6'}]
     a = 'apple'
     return render_template('test.html',a)

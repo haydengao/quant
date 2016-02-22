103ec717d98cd922dd53d11518c8ceddd51c4c9d
lixvow <lixvow@gmail.com>
2016-02-22 14:41:14 +0800

    jj

--- test.py
+++ test.py
@@ -7,4 +7,4 @@ app = Flask(__name__)
 def strategy():
     listt = [{'a':'1','b':'2','c':'3'},{'a':'4','b':'5','c':'6'}]
     a = 'apple'
-    return render_template('test.html',listt,a)
+    return render_template('test.html',a)
